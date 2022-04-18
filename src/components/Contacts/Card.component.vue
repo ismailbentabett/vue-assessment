769d37a4ad011f015f4f9cc48dd03ce013727d27
ismailbentabett <ismailbentabett@gmail.com>
2022-04-18 16:11:14 +0100

    my contacts app : deleted an extra text

--- src/components/Contacts/Card.component.vue
+++ src/components/Contacts/Card.component.vue
@@ -4,8 +4,7 @@
       v-ripple
       :elevation="hover ? 16 : 2"
       class="mx-auto cursor-pointer"
-
-      style="cursor: pointer "
+      style="cursor: pointer"
     >
       <v-img :src="contact.avatar" height="200px"></v-img>
 
@@ -17,21 +16,6 @@
         <v-icon medium color="gray darken-2"> mdi-email </v-icon>
         {{ contact.email }}
       </v-card-subtitle>
-
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-          <v-icon large color="teal darken-2"> mdi-email </v-icon>
-
-          <v-card-text>
-            I'm a thing. But, like most politicians, he promised more than he
-            could deliver. You won't have time for sleeping, soldier, not with
-            all the bed making you'll be doing. Then we'll go with that data
-            file! Hey, you add a one and two zeros to that or we walk! You're
-            going to do his laundry? I've got to find a way to escape.
-          </v-card-text>
-        </div>
-      </v-expand-transition>
     </v-card>
   </v-hover>
 </template>
